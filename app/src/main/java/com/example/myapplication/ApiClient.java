9c40a3338f89e8599ab227d537ab969572d943c0
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-07-21 21:25:09 +0530

    Understanding the internal working of recycler view

--- app/src/main/java/com/example/myapplication/ApiClient.java
+++ app/src/main/java/com/example/myapplication/ApiClient.java
@@ -4,19 +4,16 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiClient {
 
 
-
     @GET("/api/location/search/")
     Call<List<ResponseDryRun>> enterName(@Query("query") String enterName);
 
     @GET("/api/location/{woeid}/")
-
     Call<WeatherReport> enterWoeidData(@Path("woeid") long woeidNumber);
 
 //
