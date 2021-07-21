fca149abe37849ca3d0aab59fa6062ad1ef3450c
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-07-21 21:26:54 +0530

    Corrected some layout mistakes

--- app/src/main/java/com/example/myapplication/ConsolidatedWeatherItem.java
+++ app/src/main/java/com/example/myapplication/ConsolidatedWeatherItem.java
@@ -1,137 +1,137 @@
 package com.example.myapplication;
 
 import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class ConsolidatedWeatherItem implements Serializable {
 
-	@SerializedName("id")
-	private long id;
+    @SerializedName("id")
+    private long id;
 
-	@SerializedName("weather_state_name")
-	private String weatherStateName;
+    @SerializedName("weather_state_name")
+    private String weatherStateName;
 
-	public ConsolidatedWeatherItem(long id, String weatherStateName, String weatherStateAbbr, String windDirectionCompass, String created, String applicableDate, Object minTemp, Object maxTemp, Object theTemp, Object windSpeed, Object windDirection, float airPressure, int humidity, Object visibility, int predictability) {
-		this.id = id;
-		this.weatherStateName = weatherStateName;
-		this.weatherStateAbbr = weatherStateAbbr;
-		this.windDirectionCompass = windDirectionCompass;
-		this.created = created;
-		this.applicableDate = applicableDate;
-		this.minTemp = minTemp;
-		this.maxTemp = maxTemp;
-		this.theTemp = theTemp;
-		this.windSpeed = windSpeed;
-		this.windDirection = windDirection;
-		this.airPressure = airPressure;
-		this.humidity = humidity;
-		this.visibility = visibility;
-		this.predictability = predictability;
-	}
+    public ConsolidatedWeatherItem(long id, String weatherStateName, String weatherStateAbbr, String windDirectionCompass, String created, String applicableDate, Object minTemp, Object maxTemp, Object theTemp, Object windSpeed, Object windDirection, float airPressure, int humidity, Object visibility, int predictability) {
+        this.id = id;
+        this.weatherStateName = weatherStateName;
+        this.weatherStateAbbr = weatherStateAbbr;
+        this.windDirectionCompass = windDirectionCompass;
+        this.created = created;
+        this.applicableDate = applicableDate;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        this.theTemp = theTemp;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+        this.airPressure = airPressure;
+        this.humidity = humidity;
+        this.visibility = visibility;
+        this.predictability = predictability;
+    }
 
-	@SerializedName("weather_state_abbr")
-	private String weatherStateAbbr;
+    @SerializedName("weather_state_abbr")
+    private String weatherStateAbbr;
 
-	@SerializedName("wind_direction_compass")
-	private String windDirectionCompass;
+    @SerializedName("wind_direction_compass")
+    private String windDirectionCompass;
 
-	@SerializedName("created")
-	private String created;
+    @SerializedName("created")
+    private String created;
 
-	@SerializedName("applicable_date")
-	private String applicableDate;
+    @SerializedName("applicable_date")
+    private String applicableDate;
 
-	@SerializedName("min_temp")
-	private Object minTemp;
+    @SerializedName("min_temp")
+    private Object minTemp;
 
-	@SerializedName("max_temp")
-	private Object maxTemp;
+    @SerializedName("max_temp")
+    private Object maxTemp;
 
-	@SerializedName("the_temp")
-	private Object theTemp;
+    @SerializedName("the_temp")
+    private Object theTemp;
 
-	@SerializedName("wind_speed")
-	private Object windSpeed;
+    @SerializedName("wind_speed")
+    private Object windSpeed;
 
-	@SerializedName("wind_direction")
-	private Object windDirection;
+    @SerializedName("wind_direction")
+    private Object windDirection;
 
-	@SerializedName("air_pressure")
-	private float airPressure;
+    @SerializedName("air_pressure")
+    private float airPressure;
 
-	@SerializedName("humidity")
-	private int humidity;
+    @SerializedName("humidity")
+    private int humidity;
 
-	@SerializedName("visibility")
-	private Object visibility;
+    @SerializedName("visibility")
+    private Object visibility;
 
-	@SerializedName("predictability")
-	private int predictability;
+    @SerializedName("predictability")
+    private int predictability;
 
-	public long getId(){
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getWeatherStateName(){
-		return weatherStateName;
-	}
+    public String getWeatherStateName() {
+        return weatherStateName;
+    }
 
-	public String getWeatherStateAbbr(){
-		return weatherStateAbbr;
-	}
+    public String getWeatherStateAbbr() {
+        return weatherStateAbbr;
+    }
 
-	public String getWindDirectionCompass(){
-		return windDirectionCompass;
-	}
+    public String getWindDirectionCompass() {
+        return windDirectionCompass;
+    }
 
-	public String getCreated(){
-		return created;
-	}
+    public String getCreated() {
+        return created;
+    }
 
-	public String getApplicableDate(){
-		return applicableDate;
-	}
+    public String getApplicableDate() {
+        return applicableDate;
+    }
 
-	public Object getMinTemp(){
-		return minTemp;
-	}
+    public Object getMinTemp() {
+        return minTemp;
+    }
 
-	public Object getMaxTemp(){
-		return maxTemp;
-	}
-
-
-	public Object getTheTemp(){
-		return theTemp;
-	}
+    public Object getMaxTemp() {
+        return maxTemp;
+    }
 
 
-
-	public Object getWindSpeed(){
-		return windSpeed;
-	}
-
-
-	public Object getWindDirection(){
-		return windDirection;
-	}
+    public Object getTheTemp() {
+        return theTemp;
+    }
 
 
-	public float getAirPressure(){
-		return airPressure;
-	}
+    public Object getWindSpeed() {
+        return windSpeed;
+    }
 
-	public int getHumidity(){
-		return humidity;
-	}
 
-	public Object getVisibility(){
-		return visibility;
-	}
+    public Object getWindDirection() {
+        return windDirection;
+    }
 
-	public int getPredictability(){
-		return predictability;
-	}
+
+    public float getAirPressure() {
+        return airPressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public Object getVisibility() {
+        return visibility;
+    }
+
+    public int getPredictability() {
+        return predictability;
+    }
 
 }
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
@@ -24,13 +23,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements ApiViewHolder.onItemClickedListener{
+public class MainActivity extends AppCompatActivity implements ApiViewHolder.onItemClickedListener {
     private EditText etEnterId;
     private ImageView btnGetApi;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
     private ApiAdapter apiAdapter;
-    private List<ResponseDryRun> modelList=new ArrayList<>();
+    private List<ResponseDryRun> modelList = new ArrayList<>();
     private VideoView videoView;
 
 
@@ -43,23 +42,23 @@ public class MainActivity extends AppCompatActivity implements ApiViewHolder.onI
     }
 
     private void setRecyclerAdapter() {
-        apiAdapter=new ApiAdapter(modelList,this);
-        LinearLayoutManager manager=new LinearLayoutManager(this);
+        apiAdapter = new ApiAdapter(modelList, this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(apiAdapter);
 
     }
 
     private void initViews() {
-        etEnterId=findViewById(R.id.etEnterId);
-        btnGetApi=findViewById(R.id.btnRequestApi);
-        recyclerView=findViewById(R.id.recyclerView);
+        etEnterId = findViewById(R.id.etEnterId);
+        btnGetApi = findViewById(R.id.btnRequestApi);
+        recyclerView = findViewById(R.id.recyclerView);
 
 //        progressBar=findViewById(R.id.progressBar);
 //        progressBar.setVisibility(View.GONE);
-        videoView=findViewById(R.id.videoview);
+        videoView = findViewById(R.id.videoview);
 
-        Uri uri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.wathervideo_1);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.wathervideo_1);
         videoView.setVideoURI(uri);
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -70,18 +69,17 @@ public class MainActivity extends AppCompatActivity implements ApiViewHolder.onI
         videoView.start();
 
 
-
         btnGetApi.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                ApiClient apiClient=Network.getRetrofitInstance(MainActivity.this).create(ApiClient.class);
+                ApiClient apiClient = Network.getRetrofitInstance(MainActivity.this).create(ApiClient.class);
 
-                Call<List<ResponseDryRun>> call=apiClient.enterName(etEnterId.getText().toString());
+                Call<List<ResponseDryRun>> call = apiClient.enterName(etEnterId.getText().toString());
                 call.enqueue(new Callback<List<ResponseDryRun>>() {
                     @Override
                     public void onResponse(Call<List<ResponseDryRun>> call, Response<List<ResponseDryRun>> response) {
-                        if(response.code()== HttpURLConnection.HTTP_OK && response.body() != null) {
+                        if (response.code() == HttpURLConnection.HTTP_OK && response.body() != null) {
                             modelList = response.body();
                             apiAdapter.updateList(modelList);
 
@@ -93,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements ApiViewHolder.onI
 
                     @Override
                     public void onFailure(Call<List<ResponseDryRun>> call, Throwable t) {
-                        Toast.makeText(MainActivity.this,"Response Failed" + t.getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Response Failed" + t.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -104,10 +102,10 @@ public class MainActivity extends AppCompatActivity implements ApiViewHolder.onI
 
     @Override
     public void onItemClicked(int position) {
-        Toast.makeText(this, "Item Clicked at position"+ position, Toast.LENGTH_SHORT).show();
-        Intent intent=new Intent(this,Weather_Page_Activity.class);
+        Toast.makeText(this, "Item Clicked at position" + position, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Weather_Page_Activity.class);
 //            intent.putExtra("val",modelList.get(position).getWoeid());
-            intent.putExtra("data",modelList.get(position).getWoeid());
+        intent.putExtra("data", modelList.get(position).getWoeid());
 //       intent.putExtra("woeid",2487956);
         startActivity(intent);
 
